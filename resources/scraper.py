ffaa189a6f2518999ed9b81b6a3e98081a75ddaf
Piyotr-K <piyotrkao1998@outlook.com>
2021-01-19 22:25:34 -0800

    2021 Jan 16
    Signed-off-by: Piyotr-K <piyotrkao1998@outlook.com>

--- resources/scraper.py
+++ resources/scraper.py
@@ -9,16 +9,8 @@ soup = bs4.BeautifulSoup(page.content, 'html.parser')
 # print(soup.prettify())
 
 # print(list(soup.children))
-
-# print([type(item) for item in list(soup.children)])
-# ['html', '/n', 'contents of the webpage']
-
-# html = list(soup.children)[2] # Take the last item (content) in the soup and put it in a variable
-
-# body = list(html.children)[3] # Take just the body element
-
-# print(list(body.children))
-
-headings = soup.find_all('h2')
-
-print(headings[0].get_text()) # text of the element
+# Find element by tag
+# body = soup.find_all('body')
+# print(body)
+email = soup.find_all('h2')
+print(email[0].get_text().strip())
