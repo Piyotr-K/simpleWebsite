b540b2445df42afaecca490b1c5a41ad6b8e1d5d
Piyotr-K <piyotrkao1998@outlook.com>
2021-09-25 02:01:09 -0700

    2021 09 24 update
    Signed-off-by: Piyotr-K <piyotrkao1998@outlook.com>

---
+++ resources/APJava1/Intro.java
@@ -0,0 +1,45 @@
+public class Intro {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Nice");
+        System.out.print("Heehee");
+        System.out.print(" Behind it");
+        System.out.println("Always use double quotes for strings");
+        System.out.println(500);
+        System.out.println(true);
+
+        // In Python:
+        // nice = 5
+        // Need to specify type
+        int nice = 5; // primitive
+        char a = 25;
+        float f1 = 5.6f;
+        long l1 = 3;
+        double d1 = 5.6;
+        boolean help = true;
+        String sentence = "Why has god abandoned us";
+
+        int ageOfBaby;
+        ageOfBaby = 5;
+        ageOfBaby++; // ageOfBaby += 1
+        ageOfBaby--; // ageOfBaby -= 1
+        System.out.println(ageOfBaby);
+
+        // comment
+        /* comment */
+        /**
+         * Comment
+         * Comment
+         */
+
+        int choice = 1;
+
+        if (choice == 1)
+            System.out.println("Truuuu");
+        else if (choice == 2)
+            System.out.println("Help");
+        else
+            System.out.println("Something else");
+    }
+}
