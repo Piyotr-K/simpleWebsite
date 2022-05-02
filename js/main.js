6d4daa48e7fdf584b0bc7a0f6769abebb55afb33
Piyotr-K <piyotrkao1998@outlook.com>
2022-05-01 17:54:53 -0700

    2022 MAY 1 Py Algo Update
    Signed-off-by: Piyotr-K <piyotrkao1998@outlook.com>

--- js/main.js
+++ js/main.js
@@ -148,7 +148,7 @@ function setClass3()
     const endUrl = "";
     const endName = ""
     const hwTitle = "CCC Junior";
-    const hwStr = "# Required: \n# CCC2007J2\n# https://dmoj.ca/problem/ccc07j2";
+    const hwStr = "# Required: \n# CCC2007J4\n# https://dmoj.ca/problem/ccc07j4";
 
     const endAnchor = document.getElementById("end-class2").children[1].children[0];
     const hw = document.getElementById("hw-class2");
