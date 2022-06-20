9348896bd706df3e971dfa679733378ecab5d814
Piyotr-K <piyotrkao1998@outlook.com>
2022-06-19 21:50:08 -0700

    2022 JUN 19 PyAlgo Update
    Signed-off-by: Piyotr-K <piyotrkao1998@outlook.com>

--- js/main.js
+++ js/main.js
@@ -154,7 +154,7 @@ function setClass3()
     const endUrl = "";
     const endName = ""
     const hwTitle = "CCC Junior";
-    const hwStr = "# Required: \n# CCC2009J2\n# https://dmoj.ca/problem/ccc09j2";
+    const hwStr = "# Required: \n# CCC2010J1\n# https://dmoj.ca/problem/ccc10j1";
 
     const endAnchor = document.getElementById("end-class2").children[1].children[0];
     const hw = document.getElementById("hw-class2");
