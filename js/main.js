db02f6fe5a25cd15a9bbf97b218b8005e4669c31
Piyotr-K <piyotrkao1998@outlook.com>
2022-06-28 00:43:37 -0700

    2022 Java zip update
    Signed-off-by: Piyotr-K <piyotrkao1998@outlook.com>

--- js/main.js
+++ js/main.js
@@ -101,8 +101,8 @@ function setAll()
 
 function setJava3()
 {
-    const notesUrl = "resources/APJava1/2022_05_07/_end_code_/Class7.java";
-    const notesName = "Class8.zip";
+    const notesUrl = "resources/APJava1/2022_05_07/_end_code_/PigGame.zip";
+    const notesName = "PigGame.zip";
     const hwName = "Pig Game";
     let hw = `// Pig Game
 // Hw:
